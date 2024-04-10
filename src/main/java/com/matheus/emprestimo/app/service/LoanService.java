ba23358e0318410c1d1617702f51fb24bdf520cf
matheuspereira00 <matheusinhofcb12@gmail.com>
2024-04-10 09:57:35 -0300

    feat: add service

---
+++ src/main/java/com/matheus/emprestimo/app/service/LoanService.java
@@ -0,0 +1,37 @@
+package com.matheus.emprestimo.app.service;
+
+import com.matheus.emprestimo.app.dto.CustomerLoanRequest;
+import com.matheus.emprestimo.app.dto.CustomerLoanResponse;
+import com.matheus.emprestimo.app.dto.LoanResponse;
+import com.matheus.emprestimo.domain.Loan;
+import com.matheus.emprestimo.domain.LoanType;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class LoanService {
+
+    public CustomerLoanResponse checkLoanAvailability(CustomerLoanRequest loanRequest) {
+
+        var customer = loanRequest.toCustomer();
+        var loan = new Loan(customer);
+
+        List<LoanResponse> loans = new ArrayList<>();
+
+        if (loan.isPersonalLoanAvailable()) {
+            loans.add(new LoanResponse(LoanType.PERSONAL, loan.getPersonalLoanInterestRate()));
+        }
+
+        if (loan.isConsigmentLoanAvailable()) {
+            loans.add(new LoanResponse(LoanType.CONSIGNMENT, loan.getConsigmentLoanInterestRate()));
+        }
+
+        if (loan.isGuaranteedLoanAvailable()) {
+            loans.add(new LoanResponse(LoanType.GUARANTEED, loan.getGuaranteedLoanInterestRate()));
+        }
+
+        return new CustomerLoanResponse(loanRequest.name(), loans);
+    }
+}
